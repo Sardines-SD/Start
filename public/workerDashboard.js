5ee06d79a5e052b0340c26ed59a4379b22d2a11e
Kwanda Ndimande <kwandandimande23@gmail.com>
2026-04-13 11:37:06 +0200

    refactor: import firebaseConfig

--- public/workerDashboard.js
+++ public/workerDashboard.js
@@ -9,6 +9,7 @@ import {
   doc,
   getDoc,
 } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import { firebaseConfig } from './firebaseConfig.js';
 
 const app  = initializeApp(firebaseConfig);
 const auth = getAuth(app);
