72567c3cf0fc9b1786fe14a44459fdd18b8f7fa4
Kwanda Ndimande <kwandandimande23@gmail.com>
2026-04-13 11:23:03 +0200

    refactor: remove hardcoded Firebase config from js files - import from firebaseConfig.js

--- public/login.js
+++ public/login.js
@@ -9,15 +9,6 @@ import {
   getDoc,
 } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
-const firebaseConfig = {
-  apiKey:            "AIzaSyBsGq_-mPlBAfCtEt3J-SzaMQgpKmHye9E",
-  authDomain:        "municipality-50ae8.firebaseapp.com",
-  projectId:         "municipality-50ae8",
-  storageBucket:     "municipality-50ae8.firebasestorage.app",
-  messagingSenderId: "904618138528",
-  appId:             "1:904618138528:web:f4bd52b683fe6585dd62c5",
-};
-
 const app  = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db   = getFirestore(app);
